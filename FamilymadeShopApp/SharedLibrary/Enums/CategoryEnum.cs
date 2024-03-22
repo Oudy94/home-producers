620d81f7aaa94d7f83b87999ae4d751f13bda861
Saoud <saoud.bakhmais@student.fontys.nl>
2024-03-22 17:18:19 +0100

    Initialize data model

---
+++ FamilymadeShopApp/SharedLibrary/Enums/CategoryEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Enums
+{
+    public enum CategoryEnum
+    {
+        BakeryGoods,
+        BathAndBeauty,
+        ClothingAndAccessories,
+        GiftsAndSpecialOccassions,
+        CarftSupplies
+    }
+}
---
+++ FamilymadeShopApp/SharedLibrary/Models/ProductManager.cs
@@ -0,0 +1,32 @@
+﻿using SharedLibrary.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Models
+{
+    public class ProductManager
+    {
+        public Dictionary<int, Product> ProductDict { get; set; }
+
+        public ProductManager()
+        {
+            this.ProductDict = new Dictionary<int, Product>();
+        }
+
+        public void Add(int id, string name, string description, CategoryEnum category, double price, int quantity, List<string> images, int salesCount, double revenue)
+        {
+            try
+            {
+                Product product = new Product(id, name, description, category, price, quantity, images, salesCount, revenue);
+                ProductDict.Add(id, product);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
