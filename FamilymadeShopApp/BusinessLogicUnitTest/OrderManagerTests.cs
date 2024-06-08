db5d41eae6e527d49b3959255b8bb0d45f443e11
Saoud <saoud.bakhmais@student.fontys.nl>
2024-06-08 21:44:49 +0200

    Add unit tests

---
+++ FamilymadeShopApp/BusinessLogicUnitTest/OrderManagerTests.cs
@@ -0,0 +1,187 @@
+﻿using BusinessLogicLayer.Managers;
+using DataAccessLayer.Interfaces;
+using ModelLayer.Models;
+using Moq;
+using SharedLayer.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicUnitTest
+{
+    [TestClass]
+    public class OrderManagerTests
+    {
+        private Mock<IOrderRepository> _mockOrderRepository;
+        private OrderManager _orderManager;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mockOrderRepository = new Mock<IOrderRepository>();
+            _orderManager = new OrderManager(_mockOrderRepository.Object);
+        }
+
+        [TestMethod]
+        public void AddOrder_ShouldCallAddOrderDAL()
+        {
+            // Arrange
+            var order = new Order();
+
+            // Act
+            _orderManager.AddOrder(order);
+
+            // Assert
+            _mockOrderRepository.Verify(x => x.AddOrderDAL(order), Times.Once);
+        }
+
+        [TestMethod]
+        public void GetOrdersByUserId_ShouldCallGetOrdersByUserIdDAL()
+        {
+            // Arrange
+            int userId = 1;
+
+            // Act
+            _orderManager.GetOrdersByUserId(userId);
+
+            // Assert
+            _mockOrderRepository.Verify(x => x.GetOrdersByUserIdDAL(userId), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task GetOrdersCountAsync_ShouldCallGetOrdersCountAsyncDAL()
+        {
+            // Arrange
+            string filterName = "chocolate";
+            OrderStatus? filterStatus = OrderStatus.Pending;
+
+            // Act
+            await _orderManager.GetOrdersCountAsync(filterName, filterStatus);
+
+            // Assert
+            _mockOrderRepository.Verify(x => x.GetOrdersCountAsyncDAL(filterName, filterStatus), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task GetOrderDataAsync_ShouldCallGetOrderDataAsyncDAL()
+        {
+            // Arrange
+            int pageNumber = 1;
+            int pageSize = 10;
+            string filterName = "filter";
+            OrderStatus? filterStatus = OrderStatus.Pending;
+
+            // Act
+            await _orderManager.GetOrderDataAsync(pageNumber, pageSize, filterName, filterStatus);
+
+            // Assert
+            _mockOrderRepository.Verify(x => x.GetOrderDataAsyncDAL(pageNumber, pageSize, filterName, filterStatus), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task UpdateOrdersAsync_ShouldCallUpdateOrdersDAL()
+        {
+            // Arrange
+            var orders = new List<Order>();
+
+            // Act
+            await _orderManager.UpdateOrdersAsync(orders);
+
+            // Assert
+            _mockOrderRepository.Verify(x => x.UpdateOrdersDAL(orders), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task GetOrderByIdAsync_ShouldCallGetOrderByIdAsyncDAL()
+        {
+            // Arrange
+            int orderId = 1;
+
+            // Act
+            await _orderManager.GetOrderByIdAsync(orderId);
+
+            // Assert
+            _mockOrderRepository.Verify(x => x.GetOrderByIdAsyncDAL(orderId), Times.Once);
+        }
+
+        [TestMethod]
+        public void GetOrdersByUserId_ShouldReturnOrdersFromRepository()
+        {
+            // Arrange
+            int userId = 1;
+            var expectedOrders = new List<Order> { new Order(), new Order() };
+            _mockOrderRepository.Setup(x => x.GetOrdersByUserIdDAL(userId)).Returns(expectedOrders);
+
+            // Act
+            var result = _orderManager.GetOrdersByUserId(userId);
+
+            // Assert
+            CollectionAssert.AreEqual(expectedOrders, result);
+        }
+
+        [TestMethod]
+        public async Task GetOrdersCountAsync_ShouldReturnCountFromRepository()
+        {
+            // Arrange
+            string filterName = "filter";
+            OrderStatus? filterStatus = OrderStatus.Pending;
+            int expectedCount = 10;
+            _mockOrderRepository.Setup(x => x.GetOrdersCountAsyncDAL(filterName, filterStatus)).ReturnsAsync(expectedCount);
+
+            // Act
+            var result = await _orderManager.GetOrdersCountAsync(filterName, filterStatus);
+
+            // Assert
+            Assert.AreEqual(expectedCount, result);
+        }
+
+        [TestMethod]
+        public async Task GetOrderDataAsync_ShouldReturnOrdersFromRepository()
+        {
+            // Arrange
+            int pageNumber = 1;
+            int pageSize = 10;
+            string filterName = "filter";
+            OrderStatus? filterStatus = OrderStatus.Pending;
+            var expectedOrders = new List<Order> { new Order(), new Order() };
+            _mockOrderRepository.Setup(x => x.GetOrderDataAsyncDAL(pageNumber, pageSize, filterName, filterStatus)).ReturnsAsync(expectedOrders);
+
+            // Act
+            var result = await _orderManager.GetOrderDataAsync(pageNumber, pageSize, filterName, filterStatus);
+
+            // Assert
+            CollectionAssert.AreEqual(expectedOrders, result);
+        }
+
+        [TestMethod]
+        public async Task UpdateOrdersAsync_ShouldReturnTrueFromRepository()
+        {
+            // Arrange
+            var orders = new List<Order>();
+            _mockOrderRepository.Setup(x => x.UpdateOrdersDAL(orders)).ReturnsAsync(true);
+
+            // Act
+            var result = await _orderManager.UpdateOrdersAsync(orders);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public async Task GetOrderByIdAsync_ShouldReturnOrderFromRepository()
+        {
+            // Arrange
+            int orderId = 1;
+            var expectedOrder = new Order { Id = orderId };
+            _mockOrderRepository.Setup(x => x.GetOrderByIdAsyncDAL(orderId)).ReturnsAsync(expectedOrder);
+
+            // Act
+            var result = await _orderManager.GetOrderByIdAsync(orderId);
+
+            // Assert
+            Assert.AreEqual(expectedOrder, result);
+        }
+    }
+}
---
+++ FamilymadeShopApp/BusinessLogicUnitTest/ProductManagerTests.cs
@@ -0,0 +1,116 @@
+﻿using DataAccessLayer.Interfaces;
+using ModelLayer.Models;
+using Moq;
+using SharedLayer.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Managers.Tests
+{
+    [TestClass]
+    public class ProductManagerTests
+    {
+        private Mock<IProductRepository> _mockProductRepository;
+        private ProductManager _productManager;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mockProductRepository = new Mock<IProductRepository>();
+            _productManager = new ProductManager(_mockProductRepository.Object);
+        }
+
+        [TestMethod]
+        public async Task AddProductAsync_ShouldCallAddProductAsyncDAL()
+        {
+            // Arrange
+            var product = new Product();
+
+            // Act
+            await _productManager.AddProductAsync(product);
+
+            // Assert
+            _mockProductRepository.Verify(x => x.AddProductAsyncDAL(product), Times.Once);
+        }
+
+        [TestMethod]
+        public void GetProductById_ShouldReturnProductFromRepository()
+        {
+            // Arrange
+            int productId = 1;
+            var expectedProduct = new Product { Id = productId };
+            _mockProductRepository.Setup(x => x.GetProductByIdDAL(productId)).Returns(expectedProduct);
+
+            // Act
+            var result = _productManager.GetProductById(productId);
+
+            // Assert
+            Assert.AreEqual(expectedProduct, result);
+        }
+
+        [TestMethod]
+        public async Task GetAllProductsAsync_ShouldReturnProductsFromRepository()
+        {
+            // Arrange
+            int pageNumber = 1;
+            int pageSize = 10;
+            string filterName = "chocolate";
+            Category? filterCategory = Category.BakeryGoods;
+            var expectedProducts = new List<Product> { new Product(), new Product() };
+            _mockProductRepository.Setup(x => x.GetAllProductsAsyncDAL(pageNumber, pageSize, filterName, filterCategory)).ReturnsAsync(expectedProducts);
+
+            // Act
+            var result = await _productManager.GetAllProductsAsync(pageNumber, pageSize, filterName, filterCategory);
+
+            // Assert
+            CollectionAssert.AreEqual(expectedProducts, result);
+        }
+
+        [TestMethod]
+        public async Task GetProductsCountAsync_ShouldReturnCountFromRepository()
+        {
+            // Arrange
+            string filterName = "chocolate";
+            Category? filterCategory = Category.BakeryGoods;
+            int expectedCount = 10;
+            _mockProductRepository.Setup(x => x.GetProductsCountAsyncDAL(filterName, filterCategory)).ReturnsAsync(expectedCount);
+
+            // Act
+            var result = await _productManager.GetProductsCountAsync(filterName, filterCategory);
+
+            // Assert
+            Assert.AreEqual(expectedCount, result);
+        }
+
+        [TestMethod]
+        public async Task UpdateProductsAsync_ShouldReturnTrueFromRepository()
+        {
+            // Arrange
+            var products = new List<Product>();
+            _mockProductRepository.Setup(x => x.UpdateProductsAsyncDAL(products)).ReturnsAsync(true);
+
+            // Act
+            var result = await _productManager.UpdateProductsAsync(products);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public async Task GetProductsNamesAsync_ShouldReturnNamesFromRepository()
+        {
+            // Arrange
+            var expectedNames = new List<string> { "Product1", "Product2" };
+            _mockProductRepository.Setup(x => x.GetProductsNamesAsyncDAL()).ReturnsAsync(expectedNames);
+
+            // Act
+            var result = await _productManager.GetProductsNamesAsync();
+
+            // Assert
+            CollectionAssert.AreEqual(expectedNames, result);
+        }
+    }
+}
